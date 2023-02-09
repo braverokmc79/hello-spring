d903c9fc77ad4e1d1e7e0d252fba71cc7061e7e8
braverokmc79 <braverokmc79@gmail.com>
2023-02-10 00:48:49 +0900

    juit test

---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,26 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.reository.MemberRepository;
+import hello.hellospring.reository.MemoryMemberRepository;
+import org.junit.jupiter.api.Test;
+
+public class MemoryMemberRepositoryTest {
+
+    MemberRepository repository =new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+        Member member =new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result =repository.findById(member.getId()).get();
+
+
+
+
+    }
+
+}

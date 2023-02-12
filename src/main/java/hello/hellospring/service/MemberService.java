3c4d58d5d25b5ff56e8b0fb3991656b58fe74029
unknown <braverokmc79@gmail.com>
2023-02-12 22:25:15 +0900

    14.회원 서비스 테스트

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -8,7 +8,12 @@ import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
-    private  final MemberRepository memberRepository =new MemoryMemberRepository();
+    private  final MemberRepository memberRepository;
+
+    public  MemberService (MemberRepository memberRepository) {
+        this.memberRepository=memberRepository;
+    }
+
 
     /**
      * 회원가입
@@ -18,8 +23,6 @@ public class MemberService {
     public Long join(Member member){
         //같은 이름이 있는 중복 회원 x
         extracted(member);
-
-
         memberRepository.save(member);
         return member.getId();
     }

477b843340dd681fd59503490bb3062e45d0b351
unknown <braverokmc79@gmail.com>
2023-02-14 20:59:19 +0900

    19.회원 웹 기능 - 등록

--- src/main/java/hello/hellospring/controller/HomeController.java
+++ src/main/java/hello/hellospring/controller/HomeController.java
@@ -1,6 +1,7 @@
 package hello.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -9,4 +10,6 @@ public class HomeController {
     public String home(){
         return  "home";
     }
+
+
 }
--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,16 +1,30 @@
 package hello.hellospring.controller;
 
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
-
     @Autowired
     private MemberService memberService;
 
+    @GetMapping("/members/new")
+    public String createForm(){
+        return  "members/createForm";
+    }
 
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member =new Member();
+        member.setName(form.getName());
 
+        System.out.println("member =  " +member.getName());;
+        memberService.join(member);
+        return  "redirect:/";
+    }
 }
